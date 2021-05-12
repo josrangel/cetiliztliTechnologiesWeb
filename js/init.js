248072a5055cdc994138cb18afb5a676b0810894
Josue <josrangel@hotmail.com>
2021-05-12 02:33:00 -0500

    add: particles, splash, and corrections

--- js/init.js
+++ js/init.js
@@ -5,3 +5,12 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+function esconderMenu(){
+	var menu = document.getElementById("nav-mobile");
+	var sidenavOverlay = document.getElementsByClassName("sidenav-overlay");
+	menu.style.transform = "translateX(-105%)";
+	sidenavOverlay[0].style.display = "none";
+	$('.sidenav').sidenav();
+}
